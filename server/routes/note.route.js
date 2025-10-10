90565054befd53f618422e9c41299386ab6dc5bd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-10 18:28:23 +0530

    Update note.route.js

--- server/routes/note.route.js
+++ server/routes/note.route.js
@@ -6,16 +6,22 @@ const NoteController = require('../contorllers/note.controller');
 
 const router = express.Router();
 
+// create note
 router.post('/', auth, checkPermission(['note:create']), upload.single('notefile'), NoteController.createNote)
 
-router.put(':id', auth, checkPermission(['note:update']), upload.single('notefile'), NoteController.updateNote)
+// update note 
+router.put('/:id', auth, checkPermission(['note:update']), upload.single('notefile'), NoteController.updateNote)
 
-router.delete(':id', auth, checkPermission(['note:delete']), NoteController.deleteNote)
+// delete note
+router.delete('/:id', auth, checkPermission(['note:delete']), NoteController.deleteNote)
 
+// get all notes
 router.get('/', auth, checkPermission(['note:getall']), NoteController.getallnotes)
 
+// get one note
 router.get('/:id', auth, checkPermission(['note:getone']), NoteController.getOneNote)
 
+// get my notes -> this is for when current login student can access only his/her notes
 router.get('/my-notes', auth, checkPermission(['note:mynotes']), NoteController.getmyallnotes)
 
 module.exports = router;
