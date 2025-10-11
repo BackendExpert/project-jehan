076f67a3c570fdf09d2bc525f5d3569fc72cc36a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-11 07:45:57 +0530

    Update

--- client/src/component/Errors/DefultError.jsx
+++ client/src/component/Errors/DefultError.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const DefultError = () => {
     return (
         <div className="flex flex-col items-center justify-center text-center px-6 py-16">
-            <h1 className="text-9xl font-extrabold text-lime-600 animate-pulse">404</h1>
-            <h2 className="mt-4 text-3xl font-semibold tracking-wider text-lime-600">
+            <h1 className="text-9xl font-extrabold text-pink-600 animate-pulse">404</h1>
+            <h2 className="mt-4 text-3xl font-semibold tracking-wider text-pink-600">
                 Oops! Page Not Found
             </h2>
             <p className="mt-2 text-gray-600">
@@ -12,7 +12,7 @@ const DefultError = () => {
             </p>
 
             <a href="/" className="mt-8">
-                <div className="py-3 px-8 bg-lime-600 text-white rounded-full shadow-md hover:bg-lime-700 hover:shadow-lg hover:scale-105 transition duration-300">
+                <div className="py-3 px-8 bg-pink-600 text-white rounded-full shadow-md hover:bg-pink-700 hover:shadow-lg hover:scale-105 transition duration-300">
                     Go Back Home
                 </div>
             </a>
--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -4,12 +4,14 @@ import PrivateRoute from './PrivateRoute'
 import Dashboard from '../layouts/Dashboard'
 import TestInputs from '../pages/testingPages/TestInputs'
 import HomePage from '../pages/Welcome/HomePage'
+import DefultError from '../component/Errors/DefultError'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<WebSite />} >
+                    <Route path='*' element={<DefultError /> } />
                     <Route index element={<HomePage /> } />
                     <Route path='/test' element={<TestInputs />} />
                 </Route>
