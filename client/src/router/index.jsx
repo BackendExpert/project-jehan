fe6957319730065b28f7746fca1c4f5ba6ba2551
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-11 07:34:33 +0530

    Updae

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -2,18 +2,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import WebSite from '../layouts/WebSite'
 import PrivateRoute from './PrivateRoute'
 import Dashboard from '../layouts/Dashboard'
+import TestInputs from '../pages/testingPages/TestInputs'
+import HomePage from '../pages/Welcome/HomePage'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<WebSite />} >
+                    <Route index element={<HomePage /> } />
+                    <Route path='/test' element={<TestInputs />} />
                 </Route>
             </Routes>
 
-            <Route path='/Dashboard' element={<PrivateRoute element={<Dashboard /> } /> } >
+            {/* <Route path='/Dashboard' element={<PrivateRoute element={<Dashboard /> } /> } >
             
-            </Route>
+            </Route> */}
         </BrowserRouter>
     )
 }
