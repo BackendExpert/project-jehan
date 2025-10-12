ee4de71f10bd0d55eb981b3ac288723fccc9dda2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-12 06:43:38 +0530

    Update auth.route.js

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -3,8 +3,22 @@ const AuthController = require('../contorllers/auth.controller');
 
 const router = express.Router();
 
+// registaion
 router.post('/registation', AuthController.registaion)
+
+// verify email
 router.post('/verify-email', AuthController.verifyEmail)
+
+// login
 router.post('/login', AuthController.login)
+
+// forgetpassword
+router.post('/forget-password', AuthController.forgetPassword)
+
+// verify-otp
+router.post('/verify-opt', AuthController.verifyotp)
+
+// update password
+router.post('/update-password', AuthController.updatePassword)
 
 module.exports = router;
