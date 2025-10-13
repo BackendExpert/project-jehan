c86b71a83f7d1d16992676ba0d102b9a31b8fc29
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-14 04:13:42 +0530

    Update UpdateRoleUser.jsx

--- client/src/pages/Dashboard/Users/UpdateRoleUser.jsx
+++ client/src/pages/Dashboard/Users/UpdateRoleUser.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import API from '../../../service/api'
 
 const UpdateRoleUser = () => {
-    const {id} = useParams()
+    const { id } = useParams()
+    const [allroles, setallroles] = useState([]);
+    const [search, setSearch] = useState("");
+    const [error, setError] = useState(null);
+    const token = localStorage.getItem("token");
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const res = await API.get(`/user/roledata?nocache=${Date.now()}`, {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                setallroles(Array.isArray(res.data.result) ? res.data.result : []);
+            } catch (err) {
+                console.error("Failed to fetch Roles:", err);
+                setallroles("Failed to load Roles.");
+            }
+        };
+        fetchUsers();
+    }, [token]);
+
+    
+
     return (
-        <div>UpdateRoleUser : {id}</div>
+        <div>
+
+        </div>
     )
 }
 
